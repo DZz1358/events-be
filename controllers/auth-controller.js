8af8871ad2718d953f22f86321c21bb36c6ad623
Den <saloed2206@gmail.com>
2024-06-12 13:52:15 +0300

    added registration and send to email

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,27 +1,61 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const { google } = require('googleapis');
+const nodemailer = require('nodemailer')
+
+const myEmail = 'yourbestfriend1587@gmail.com'
+
+const CLIENT_ID = '33473790718-hgqun8ant77obrm65k1ol1rtdt432s9r.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = "1//04mgUVrKv8fIFCgYIARAAGAQSNwF-L9IrZmgrBgJVNy_llUGD9SxxF5-0OcRZLNTCLr5Mi14e4oeZ3sLkH5jD707XQHawFUFCTIo";
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 exports.register = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, name } = req.body;
+
+
+    if (!email || !name) {
+        return res.status(400).send('Email и имя обязательны.');
+    }
+
+    // Генерация случайного пароля
+    const password = Math.random().toString(36).slice(-8);
 
     try {
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
+        // Создание нового пользователя и сохранение в базе данных
+        const newUser = new User({ email, name, password });
+        await newUser.save();
 
-        user = new User({ email, password });
+        const { token } = await oAuth2Client.getAccessToken();
 
-        // const salt = await bcrypt.genSalt(10);
-        // user.password = await bcrypt.hash(password, salt);
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: myEmail,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: token,
+            },
+        });
 
-        await user.save();
+        const mailOptions = {
+            from: myEmail,
+            to: email,
+            subject: 'Ваш пароль для регистрации',
+            text: `Привет, ${name}! Ваш пароль: ${password}`,
+        };
 
-        res.status(201).json({ message: 'User created' });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Сообщение с паролем отправлено на ваш email.');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Ошибка при отправке email.');
     }
 };
 
@@ -33,11 +67,6 @@ exports.login = async (req, res) => {
         if (!user) {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
-
-        // const isPasswordValid = await bcrypt.compare(password, user.password);
-        // if (!isPasswordValid) {
-        //     return res.status(400).json({ message: 'Invalid email or password' });
-        // }
 
         const token = jwt.sign({ userId: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
         res.json({ token });
